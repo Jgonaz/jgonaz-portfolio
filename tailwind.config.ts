d5ecc2257505dd4f415c9a08b8e237984eb1405e
Jorge González <jgonaz.dev@gmail.com>
2024-01-11 20:35:51 +0100

    Initial config, color theme, profile photo.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,17 +4,28 @@ const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        jade: {
+          '50': '#effef7',
+          '75': '#e4fef3',
+          '100': '#dafeef',
+          '200': '#b8fadd',
+          '300': '#81f4c3',
+          '400': '#43e5a0',
+          '500': '#1acd81',
+          '600': '#0fa968',
+          '700': '#108554',
+          '800': '#126945',
+          '900': '#11563a',
+          '950': '#03301f'
+        }
       },
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
 export default config
